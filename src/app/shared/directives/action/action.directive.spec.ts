a07f439b499e9423ed8d35cd777ddb024d24c750
Everton <everton.walker@hotmail.com>
2021-03-19 14:58:48 -0300

    debug element in test

--- src/app/shared/directives/action/action.directive.spec.ts
+++ src/app/shared/directives/action/action.directive.spec.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { ActionDirectiveModule } from './action-directive.module';
+import { ActionDirective } from './action.directive';
 
 describe('Action', () => {
   let fixture: ComponentFixture<ActionDirectiveTestComponent>;
@@ -17,11 +19,21 @@ describe('Action', () => {
   });
 
   it('(DOM) (@Output appAction) should emit event  with payload when ENTER key is pressed', () => {
+    // Existe também o debugElement, ele vem com mais recursos de busca pra encontrar elementos,
+    // No caso aqui embaixo a gente procura o elemento que usa a diretiva que está assoaciado ao componente e temos que pegar o
+    // nativeElement pq ele retorna um debugElement e não um nativeElement por padrão.
+    const div = fixture.debugElement.query(By.directive(ActionDirective))
+      .nativeElement;
+    const event = new KeyboardEvent('keyup', { key: 'Enter' });
+    div.dispatchEvent(event);
+    expect(component.hasEvent()).toBe(true);
+  });
+
+  it('(DOM) (@Output appAction) should emit event  with payload when click is trigged', () => {
     const div: HTMLElement = fixture.nativeElement.querySelector(
       '.dummy-component'
     );
-
-    const event = new KeyboardEvent('keyup', { key: 'Enter' });
+    const event = new Event('click');
     div.dispatchEvent(event);
     expect(component.hasEvent()).toBe(true);
   });
